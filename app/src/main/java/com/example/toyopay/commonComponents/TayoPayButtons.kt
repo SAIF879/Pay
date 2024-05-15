9ca91470caf61d078f165754cda4ebc65620b20a
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2024-05-15 13:07:00 +0530

    added in assets and making components

---
+++ app/src/main/java/com/example/toyopay/commonComponents/TayoPayButtons.kt
@@ -0,0 +1,48 @@
+package com.example.toyopay.commonComponents
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Text
+import com.example.toyopay.ui.theme.navyBlue
+
+@Preview
+@Composable
+fun GenerateFunctionalButton(
+    text: String = "Get Started",
+    horizontalPadding: Int = 15,
+    verticalPadding: Int=10,
+    isEnabled: Boolean = true,
+    modifier: Modifier = Modifier,
+    height : Int = 72,
+    onClick: () -> Unit = {}
+) {
+    Button(
+        onClick = { onClick.invoke() },
+        modifier = modifier
+            .fillMaxWidth()
+            .height(height.dp)
+            .padding(horizontalPadding.dp, verticalPadding.dp),
+
+        colors = ButtonDefaults.buttonColors(
+            containerColor = navyBlue,
+            disabledContainerColor = Color.Transparent,
+        ),
+        shape = RoundedCornerShape(20.dp),
+        enabled = isEnabled
+    ) {
+        Text(text = text)
+
+//        if (isEnabled){
+//            CaratrakTexts.MainButtonText(text = text , color = PaleGold)
+//        }
+    }
+}
