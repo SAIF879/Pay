cafafc5d317edb1968937f13614c362ebe183ec0
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2024-05-15 14:04:17 +0530

    splash screen done

--- app/src/main/java/com/example/toyopay/mainflow/splash/ui/SplashScreen.kt
+++ app/src/main/java/com/example/toyopay/mainflow/splash/ui/SplashScreen.kt
@@ -1,2 +1,28 @@
 package com.example.toyopay.mainflow.splash.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.toyopay.R
+import com.example.toyopay.commonComponents.TayoPayBackground
+import com.example.toyopay.commonComponents.TayoPayIcon
+
+
+@Preview
+@Composable
+fun SplashScreen() {
+    TayoPayBackground {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TayoPayIcon(icon = R.drawable.icon_tayo_pay, size = 200)
+
+        }
+    }
+}
