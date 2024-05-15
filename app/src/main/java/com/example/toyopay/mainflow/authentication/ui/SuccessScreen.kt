a0323b6dacc4cd1db77629bdd911992f144355b6
Mohd Saif Ahmed <saifahmed9308@gmail.com>
2024-05-15 15:11:33 +0530

    components for authentication

--- app/src/main/java/com/example/toyopay/mainflow/authentication/ui/SuccessScreen.kt
+++ app/src/main/java/com/example/toyopay/mainflow/authentication/ui/SuccessScreen.kt
@@ -1,2 +1,42 @@
 package com.example.toyopay.mainflow.authentication.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.toyopay.commonComponents.GenerateFunctionalButton
+import com.example.toyopay.commonComponents.TayoPayBackground
+import com.example.toyopay.commonComponents.TayoPayTexts
+import com.example.toyopay.ui.theme.NavyBlue
+
+@Preview
+@Composable
+fun SuccessScreen(){
+    TayoPayBackground {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            TayoPayTexts.TextAsBoldHeader("SignUp Success !!" , color = Color.Black)
+            Spacer(modifier = Modifier.height(10.dp))
+            TayoPayTexts.TextAsSmallHeader(text = "Your account has been \n created successfully" , color = NavyBlue)
+            Spacer(modifier = Modifier.height(30.dp))
+            GenerateFunctionalButton("Login"){
+
+            }
+
+        }
+    }
+}
